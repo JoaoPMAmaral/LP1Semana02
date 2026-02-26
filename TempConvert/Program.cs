1e310001334f1609bfeaaf357d3c0bd213aad69c
Joao A <joao.pedro.matias.amaral@gmail.com>
2026-02-26 16:16:02 +0000

    does the conversion and prints it

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -11,6 +11,22 @@ namespace TempConvert
 
             Console.Write("Unit(C/F): ");
             string units = Console.ReadLine();
+            
+            double convertedNumber;
+            double temp = double.Parse(temperature);
+
+            switch (units)
+            {
+                case "C":
+                convertedNumber = temp * 1.8 + 32;
+                Console.WriteLine($"{temperature} C = {convertedNumber:f2} F");
+                break;
+
+                case "F":
+                convertedNumber = (temp-32)/1.8;
+                Console.WriteLine($"{temperature} F = {convertedNumber:f2} C");
+                break;
+            }
         }
     }
 }
