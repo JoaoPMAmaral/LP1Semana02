19ec668097a65159a62950eb3fb0636ca1abd9d4
Joao A <joao.pedro.matias.amaral@gmail.com>
2026-02-26 16:38:08 +0000

    forgot to swap temp for convertedNumber in absolute, colossal mistake

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -20,13 +20,13 @@ namespace TempConvert
                 case "C":
                 convertedNumber = temp * 1.8 + 32;
                 Console.WriteLine($"{temp:f2} C = {convertedNumber:f2} F");
-                Console.WriteLine($"Absolute value: {Math.Abs(temp):f2}");
+                Console.WriteLine($"Absolute value: {Math.Abs(convertedNumber):f2}");
                 break;
 
                 case "F":
                 convertedNumber = (temp-32)/1.8;
                 Console.WriteLine($"{temp:f2} F = {convertedNumber:f2} C");
-                Console.WriteLine($"Absolute value: {Math.Abs(temp):f2}");
+                Console.WriteLine($"Absolute value: {Math.Abs(convertedNumber):f2}");
                 break;
 
                 default:
