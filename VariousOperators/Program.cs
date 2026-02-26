34256ea3984fddbca78856343fb23aab2ab28b71
Joao A <joao.pedro.matias.amaral@gmail.com>
2026-02-26 15:36:54 +0000

    updated solution & added variousOperators

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insere número inteiro-não negativo: ");
+            string numeroInteiroNaoNegativo = Console.ReadLine();
+            byte n = byte.Parse(numeroInteiroNaoNegativo);
+            Console.WriteLine(n/2);
+            Console.WriteLine(n<<3);
+            Console.WriteLine(n^6);
+            Console.WriteLine(n>10);
+        }
+    }
+}
