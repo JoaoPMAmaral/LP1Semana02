03bc71a037ce271e25ce24d495103cf5b03d6ef3
Joao A <joao.pedro.matias.amaral@gmail.com>
2026-02-26 15:28:11 +0000

    updated solution & added smoothoperator

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insere número inteiro: ");
+            string numero = Console.ReadLine();
+            sbyte n = sbyte.Parse(numero);
+            n--;
+            Console.WriteLine(n);
+            sbyte numeroInt = ++n;
+            Console.WriteLine(numeroInt);
+        }
+    }
+}
